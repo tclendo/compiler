ead20e48a7eeb3d8c37f96cadf9c7b89dd4de95a
tclendo <33107696+tclendo@users.noreply.github.com>
2021-01-14 21:17:52 -0800

    Move precedences to SyntaxFacts

--- Forte/CodeAnalysis/Parser.cs
+++ Forte/CodeAnalysis/Parser.cs
@@ -133,7 +133,7 @@ namespace Forte.CodeAnalysis
 
             while (true) {
 
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence) {
 
                     break;
@@ -145,27 +145,6 @@ namespace Forte.CodeAnalysis
             }
 
             return left;
-        }
-
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind) {
-
-            /*
-                Helps get precedence of different operators
-            */
-            
-            switch (kind) {
-
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-                
-                default:
-                    return 0;
-            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression() {
